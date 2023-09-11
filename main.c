8a5112d7f303383be672ef61f297bb79bac920a6
Jack <96661942+jpshrader@users.noreply.github.com>
2023-09-11 15:59:51 -0400

    yeah

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
-int main() {
-    printf("Hello World!\n");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s name\n", argv[0]);
+        return 1;
+    }
+
+    printf("Hello, %s!\n", argv[1]);
+
+    return 0;
 }
